a6e6510ef09e8ff2e8e7c94aa47b852e84907c0f
francescobianco <info.francescobianco@gmail.com>
2017-08-21 01:56:02 +0200

    introducing test framework

--- tests/Syntax/NewlineTest.php
+++ tests/Syntax/NewlineTest.php
@@ -1,6 +1,22 @@
 <?php
-require_once '../../python/python.build.php';
-require_once '../../phpython.php';
 
-phpython::import('test2.py');
+namespace Phpython\Tests\Syntax;
 
+use Javanile\Producer;
+use PHPUnit\Framework\TestCase;
+use Phpython\Interpreter;
+
+Producer::addPsr4(['Phpython\\Tests\\' => __DIR__]);
+
+final class NewlineTest extends TestCase
+{
+    public function testOnlyOneNewline()
+    {
+        ## require_once '../../python/python.build.php';
+        ## require_once '../../phpython.php';
+        $interpreter = new Interpreter();
+
+        ## phpython::import('test2.py');
+        $output = $interpreter->run(__DIR__.'/NewlineTest1.py');
+    }
+}
